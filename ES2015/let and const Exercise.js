3a42fd67ec61def5282540a477939e06f85380a0
RivkaSusi <151357132+RivkaSusi@users.noreply.github.com>
2024-01-08 13:59:06 +0000

    Add files via upload

---
+++ ES2015/let and const Exercise.js
@@ -0,0 +1,20 @@
+/*
+
+// ES2015 Global Constants 
+var PI = 3.14 
+
+>>
+
+const PI = 3.14 
+
+// Quiz 
+
+1. With var you can reassign and redeclare and access a hoisted variable. With let you cannot redecalre and let creates block scope.
+
+2. With var you can reassign and redeclare and access a hoisted variable. With const you cannot redeclare or reassign and const creates block scope.
+
+3. With let you can reassign, with const you cannot redeclare or reassign. 
+
+4. Variables are hoisted to the top of the scope they are declared in. Therefore when using var you can access the variable name and it's undefined balue before it is used later on. Function declarations are also hoisted. 
+
+*/
