e2a97231d1e24afa41dcdc51550b48f2a3e1aea6
RivkaSusi <151357132+RivkaSusi@users.noreply.github.com>
2024-01-14 15:01:35 +0000

    Add files via upload

---
+++ Fruit Autocomplete Project/Fruit Search.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	return fruit.filter(item => item.toLowerCase().includes(str.toLowerCase()));
+}
+
+function searchHandler(e) {
+	const searchInput = e.target.value;
+	const results = search(searchInput);
+	showSuggestions(results, searchInput);
+}
+
+function showSuggestions(results, searchInput) {
+	suggestions.innerHTML = '';
+	if(searchInput.length === 0){
+		suggestions.style.display = 'none';
+		return;
+	}
+
+	for(let result of results){
+		const li = document.createElement('li');
+		li.textContent = result;
+		suggestions.appendChild(li);
+	}
+	suggestions.style.display = 'block';
+}
+
+function useSuggestion(e) {
+	const selectedSuggestion = e.target.textContent;
+	input.value = selectedSuggestion;
+	suggestions.style.display = 'none';
+}
+
+    input.addEventListener('keyup', searchHandler);
+    suggestions.addEventListener('click', useSuggestion);
